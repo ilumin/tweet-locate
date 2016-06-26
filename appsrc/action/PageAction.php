3c69d850dfe9b4f11eab181be877fcc803dfef93
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-06-26 21:11:11 +0700

    ADD simple template

--- appsrc/action/PageAction.php
+++ appsrc/action/PageAction.php
@@ -3,11 +3,21 @@ namespace App\Action;
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use Slim\Views\Twig;
 
 class PageAction
 {
+    /**
+     * @var Twig
+     */
+    public $view;
+
+    public function __construct($view)
+    {
+        $this->view = $view;
+    }
+
     public function main(Request $request, Response $response, $args = []) {
-        $data[] = 'Yike!';
-        return $response->withJson($data);
+        return $this->view->render($response, 'main.twig');
     }
 }
