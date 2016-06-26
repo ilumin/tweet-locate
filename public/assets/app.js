b777a536e117e49fa00de810d83398667dc84f0c
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-06-26 23:15:59 +0700

    ADD gmap search by address

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,5 +1,41 @@
 (function() {
 
-    console.log('Yike!');
+    var app = {
+        map: null,
+        init: function() {
+            this.map = new GMaps({
+                div: '#map',
+                lat: 13.7563,
+                lng: 100.5018
+            });
+        },
+        setCenter: function(lat, lng) {
+            this.map.setCenter(lat, lng);
+        },
+        addMarker: function(lat, lng, markerOption) {
+            this.map.addMarker({
+                lat: lat,
+                lng: lng
+            });
+        }
+    };
+
+    app.init();
+
+    $('#find-tweet').submit(function(e){
+        e.preventDefault();
+
+        GMaps.geocode({
+            address: $('#city').val().trim(),
+            callback: function(results, status) {
+                if (status!='OK') {
+                    return;
+                }
+
+                var latlng = results[0].geometry.location;
+                app.setCenter(latlng.lat(), latlng.lng());
+            }
+        });
+    });
 
 })();
