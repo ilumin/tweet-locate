521cfbe3749e7ddbf89bdf7ec0b2cdf47daf1470
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2016-06-26 12:02:14 +0700

    FIX skeleton mv constant line mv php config to first line

--- bootstrap/settings.php
+++ bootstrap/settings.php
@@ -1,10 +1,4 @@
 <?php
-// TODO move to development environment
-// Display error on development environment
-error_reporting(E_ALL);
-ini_set('display_errors', 'on');
-// Disable opcache on development environment
-ini_set('opcache.enable', '0');
 
 // TODO separate config into environment
 return [
--- public/index.php
+++ public/index.php
@@ -1,6 +1,13 @@
 <?php
-require VENDOR . '/autoload.php';
+// TODO move to development environment
+// Display error on development environment
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+// Disable opcache on development environment
+ini_set('opcache.enable', '0');
+
 require '../bootstrap/constants.php';
+require VENDOR . '/autoload.php';
 
 $app = new \Slim\App( require BOOTSTRAP . '/settings.php' );
 require BOOTSTRAP . '/dependencies.php';
